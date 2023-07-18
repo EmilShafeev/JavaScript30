0581b7d801e3e514d76510e770ad2acb04ec50a4
Emil Shafeev <amil0512@gmail.com>
2023-07-18 16:24:25 +0500

    Day 19 - skiped

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,12 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector('.player')
+const canvas = document.querySelector('.photo')
+const ctx = canvas.getContext('2d')
+const strip = document.querySelector('.strip')
+const snap = document.querySelector('.snap')
+
+function takePhoto() {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObject = stream
+        video.play()
+    })
+}
